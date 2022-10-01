6e20fb9460798707047d5ee9301a890e65ddfc1b
Zeyu <liuzeyu246@gmail.com>
2022-09-30 22:18:57 -0400

    Initial commit

---
+++ src/main/java/com/example/staybooking/controller/CustomExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.example.staybooking.controller;
+
+import com.example.staybooking.exception.StayNotExistException;
+import com.example.staybooking.exception.UserAlreadyExistException;
+import com.example.staybooking.exception.UserNotExistException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class CustomExceptionHandler {
+
+    @ExceptionHandler(UserAlreadyExistException.class)
+    public final ResponseEntity<String> handleUserAlreadyExistExceptions(Exception ex, WebRequest request) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(UserNotExistException.class)
+    public final ResponseEntity<String> handleUserNotExistExceptions(Exception ex, WebRequest request) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
+
+    }
+
+    @ExceptionHandler(StayNotExistException.class)
+    public final ResponseEntity<String> handleStayNotExistExceptions(Exception ex, WebRequest request) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+}
---
+++ src/main/java/com/example/staybooking/repository/StayRepository.java
@@ -0,0 +1,15 @@
+package com.example.staybooking.repository;
+
+import com.example.staybooking.model.Stay;
+import com.example.staybooking.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface StayRepository extends JpaRepository<Stay, Long> {
+    List<Stay> findByHost(User user);
+
+    Stay findByIdAndHost(Long id, User host);
+}
